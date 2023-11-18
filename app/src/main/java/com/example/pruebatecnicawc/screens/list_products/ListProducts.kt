17cb9a226318b58341dcc71bfd5627f125cb4caf
luisfe12 <luisfevf12@gmail.com>
2023-11-18 08:36:56 -0500

    creacion lazycolumn(recyclerview) de productos

--- app/src/main/java/com/example/pruebatecnicawc/screens/list_products/ListProducts.kt
+++ app/src/main/java/com/example/pruebatecnicawc/screens/list_products/ListProducts.kt
@@ -2,10 +2,16 @@
 
 package com.example.pruebatecnicawc.screens.list_products
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
@@ -13,6 +19,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -24,6 +31,17 @@ import com.example.pruebatecnicawc.viewmodel.ProductsViewModel
 @Composable
 fun ListProducts(navController: NavHostController, productsViewModel: ProductsViewModel) {
     val products by productsViewModel.products.collectAsStateWithLifecycle()
+    if (products.isNotEmpty()) {
+        LazyVerticalGrid(columns = GridCells.Fixed(2)) {
+            items(products) {
+
+            }
+        }
+    }else{
+        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+            CircularProgressIndicator()
+        }
+    }
 }
 
 
@@ -34,12 +52,13 @@ fun SerachProductdTextField(name: String, productsViewModel: ProductsViewModel) 
         onValueChange = { productsViewModel.changeNameProductToSearch(it) },
         singleLine = true,
         label = { Text(text = "Producto", fontSize = 15.sp, fontWeight = FontWeight.SemiBold) },
-        placeholder = { Text(text = "Buscar Producto")},
+        placeholder = { Text(text = "Buscar Producto") },
         trailingIcon = {
             Icon(imageVector = Icons.Default.Search, contentDescription = null)
         },
         colors = TextFieldDefaults.outlinedTextFieldColors(),
         modifier = Modifier
             .fillMaxWidth()
-            .padding(start = 15.dp, end = 15.dp))
+            .padding(start = 15.dp, end = 15.dp)
+    )
 }
